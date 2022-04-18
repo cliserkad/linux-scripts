2cb1492dee7e751099347ed50ec3759bb7469270
cliserkad <bryanchristopherjohnson@gmail.com>
2022-04-18 13:45:57 -0400

    add all operators to nums.sh

--- group1/nums.sh
+++ group1/nums.sh
@@ -4,5 +4,16 @@ read -p "Enter #1: " num1
 read -p "Enter #2: " num2
 
 sum=`expr $num1 + $num2`
-
 echo "The sum is: $sum"
+
+product=`expr $num1 \* $num2`
+echo "The product is: $product"
+
+difference=`expr $num1 - $num2`
+echo "The difference is: $difference"
+
+quotient=`expr $num1 / $num2`
+echo "The quotient is: $quotient"
+
+remainder=`expr $num1 % $num2`
+echo "The remainder is: $remainder"
