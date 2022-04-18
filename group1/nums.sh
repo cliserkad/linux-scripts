704efc4ea597e8ba1c6ccaa98923a3ee4e49c73a
cliserkad <bryanchristopherjohnson@gmail.com>
2022-04-18 14:03:41 -0400

    adjust format of nums.sh

--- group1/nums.sh
+++ group1/nums.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-read -p "Enter #1: " num1
-read -p "Enter #2: " num2
+read -p "Enter #1:   " num1
+read -p "Enter #2:   " num2
 
 sum=`expr $num1 + $num2`
-echo "The sum is: $sum"
+echo "sum:        $sum"
 
 product=`expr $num1 \* $num2`
-echo "The product is: $product"
+echo "product:    $product"
 
 difference=`expr $num1 - $num2`
-echo "The difference is: $difference"
+echo "difference: $difference"
 
 quotient=`expr $num1 / $num2`
-echo "The quotient is: $quotient"
+echo "quotient:   $quotient"
 
 remainder=`expr $num1 % $num2`
-echo "The remainder is: $remainder"
+echo "remainder:  $remainder"
