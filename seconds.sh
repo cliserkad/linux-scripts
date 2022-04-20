b73453f505e8066bc6719839876520a06f5fa2ef
cliserkad <bryanchristopherjohnson@gmail.com>
2022-04-20 00:00:19 -0400

    add seconds.sh

---
+++ seconds.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# get input from user. "t" for time
+read -p "Enter time in seconds: " t
+
+# print output as hours + minutes + seconds
+printf "%d hours \n%d minutes \n%d seconds \n" $((t/3600)) $((t%3600/60)) $((t%60))
